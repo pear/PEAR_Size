4c4944caf0f2d5461de02f769800fb6b31def8b9
kguest <kguest@c90b9560-bf6c-de11-be94-00142212c4b1>
2008-03-27 22:25:37 +0000

    tweak

--- Size/Size_Factory.php
+++ Size/Size_Factory.php
@@ -21,7 +21,7 @@
  * @author    Ken Guest <ken@linux.ie>
  * @copyright 2008 Ken Guest
  * @license   LGPL (see http://www.gnu.org/licenses/lgpl.html)
- * @version   CVS: <cvs_id>
+ * @version   CVS: $Id$
  * @link      http://pear.php.net/package/PEAR_Size
  */
 interface PEAR_SIZE_Factory
@@ -43,7 +43,7 @@ interface PEAR_SIZE_Factory
  * @author    Ken Guest <ken@linux.ie>
  * @copyright 2008 Ken Guest
  * @license   LGPL (see http://www.gnu.org/licenses/lgpl.html)
- * @version   CVS: <cvs_id>
+ * @version   CVS: $Id$
  * @link      http://pear.php.net/package/PEAR_Size
  */
 interface PEAR_Size_Output_Driver
